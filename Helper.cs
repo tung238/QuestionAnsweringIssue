85c97b9bfa3ac3d2d9c4bf532d205128ab6cfbee
Tung Tran <tung238@gmail.com>
2022-08-05 11:19:00 +0700

    write benchmark

---
+++ Helper.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionAnsweringIssue
+{
+    public static class Helper
+    {
+        public static RequestContent GetDeleteSourceRequest(string tenantId)
+        {
+            var model = new 
+            {
+                Op = "delete",
+                Value = new 
+                {
+                    Source = tenantId
+                }
+            };
+            return RequestContent.Create(new List<object> { model });
+        }
+
+        public static RequestContent ToUpdateQnasRequest(this List<Content> contents, string operation)
+        {
+            var items = contents.Select(b => b.ToRequestItem(operation));
+            return RequestContent.Create(items);
+        }
+
+        public static object ToRequestItem(this Content block, string operation)
+        {
+            return new 
+            {
+                Op = operation,
+                Value = new
+                {
+                    Source = block.TenantId,
+                    Answer = block.Answer,
+                    Questions = block.Questions,
+                    Metadata = new Dictionary<string, string> {
+                    { "ContentId", block.Id } ,
+                    { "TenantId", block.TenantId }
+                }
+                }
+            };
+        }
+    }
+
+    public class Content
+    {
+        public string Id { get; set; }
+        public string TenantId { get; set; }
+        public string Answer { get; set; }
+        public string[] Questions { get; set; }
+    }
+}
---
+++ MyBenchmarks.cs
@@ -0,0 +1,59 @@
+﻿using Azure;
+using Azure.AI.Language.QuestionAnswering;
+using Azure.AI.Language.QuestionAnswering.Projects;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace QuestionAnsweringIssue
+{
+    public class MyBenchmarks
+    {
+        private QuestionAnsweringClient client;
+        private QuestionAnsweringProjectsClient projectsClient;
+        private string NewString => Guid.NewGuid().ToString();
+        private string ProjectName => "TrabbleKnowledgebaseTest";
+        public MyBenchmarks()
+        {
+            var endpoint = new Uri("");
+            var credential = new AzureKeyCredential("");
+            client = new QuestionAnsweringClient(endpoint, credential);
+            projectsClient = new QuestionAnsweringProjectsClient(endpoint, credential);
+        }
+
+        [Benchmark]
+        public async Task UpdateSourcesAsync()
+        {
+            var request = Helper.GetDeleteSourceRequest(NewString);
+            await projectsClient.UpdateSourcesAsync(WaitUntil.Completed, ProjectName, request);
+        }
+
+        [Benchmark]
+        public async Task UpdateQnasAsync()
+        {
+            var tenantId = NewString;
+            var blocks = new List<Content>
+            {
+                new Content {Answer = NewString, Id = NewString, Questions = new string[]{NewString}, TenantId = tenantId},
+                new Content {Answer = NewString, Id = NewString, Questions = new string[]{NewString}, TenantId = tenantId},
+                new Content {Answer = NewString, Id = NewString, Questions = new string[]{NewString}, TenantId = tenantId},
+                new Content {Answer = NewString, Id = NewString, Questions = new string[]{NewString}, TenantId = tenantId}
+            };
+            var request = blocks.ToUpdateQnasRequest("add");
+            await projectsClient.UpdateQnasAsync(WaitUntil.Completed, ProjectName, request);
+        }
+
+        [Benchmark]
+        public async Task DeployProjectAsync()
+        {
+            await projectsClient.DeployProjectAsync(WaitUntil.Completed, ProjectName, "production");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MyBenchmarks>();
+        }
+    }
+}
